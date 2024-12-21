7f43998bc53c1ac6e7457d2baeee3ecb6855e76b
yousseftaher65 <yousseftaher65@gmail.com>
2024-12-21 07:19:37 +0200

    app init

---
+++ README.md
@@ -0,0 +1,3 @@
+# event_planning_pojo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/event_planning_pojo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.event_planning_pojo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
